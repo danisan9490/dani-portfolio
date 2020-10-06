ecf81a2981e97829d46eb13dc940523cbd6b1287
Daniel <danisan9490@gmail.com>
2020-10-06 10:48:09 +0100

    #1 Install React and Create Navbar

---
+++ portfolio/src/components/Navbar/MenuItem.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links',
+    target: false
+  },
+  {
+    title: 'Projects',
+    url: '#',
+    cName: 'nav-links',
+    target: false
+  },
+  {
+    title: 'About Me',
+    url: '#',
+    cName: 'nav-links',
+    target: false
+  },
+  {
+    title: 'Contact',
+    url: '#',
+    cName: 'nav-links',
+    target: false
+  },
+  {
+    title: 'GitHub',
+    url: 'https://github.com/danisan9490',
+    cName: 'nav-links',
+    target: true
+  }
+]
---
+++ portfolio/src/components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { MenuItems } from './MenuItem'
+import './Navbar.css';
+
+function Navigation() {
+
+  const [navbar, setNavBar] = useState(false);
+
+  const handleClick = (e) => {
+    navbar ? setNavBar(false) : setNavBar(true);
+  }
+
+  return (
+    <nav className="navbarItems">
+      <h1 className="navbar-logo">Dani<i className="fab fa-react"></i>Portfolio</h1>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={navbar ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={navbar ? 'nav-menu active' : 'nav-menu'}>
+        {
+          MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a
+                  className={item.cName}
+                  href={item.url}
+                  target={item.target ? "_blank" : null}
+                  rel="noopener noreferrer"
+                >
+                  {item.title}
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </nav >
+  );
+}
+
+export default Navigation;
+
