a0d4f288e1194ec26e6cd075a12860a51bfcfc3c
Daniel <danisan9490@gmail.com>
2020-10-06 12:09:32 +0100

    #4 Create Background and Fix NavBar

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './Home.css';
 import Logo from './Logo/Logo';
+import Particles from 'react-particles-js';
 
-
+const particulesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 
 function Home() {
   return (
     <div className="Home">
-      <h3>Homee</h3>
       <Logo />
+      <Particles
+        className="particles"
+        params={particulesOptions}
+      />
+
     </div>
   );
 }
