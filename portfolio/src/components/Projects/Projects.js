675bd5a2ad9fc551f04b5fb2a22024099829e3b9
Daniel <danisan9490@gmail.com>
2020-10-09 07:24:07 +0100

    #9 Create Projects Buttons

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import Card from './Card/Card';
 import './Projects.css';
+// import Particles from 'react-particles-js';
 
+// const particulesOptions = {
+//   particles: {
+//     number: {
+//       value: 50,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       }
+//     }
+//   }
+// }
 
 function Projects({ projects }) {
   return (
-    <div className="projects">
-      {projects.length > 0 ? projects[0].map((project, i) => {
-        return <Card
-          key={i}
-          title={project.title}
-          url={project.url}
-          description={project.description}
-          lifeWeb={project.lifeWeb}
-          img={project.img}
-          category={project.category}
-        />
-      }) :
-        null
-      }
+    <div className="container">
+      <div className="categories">
+        <button className="button" >Full Stack</button>
+        <button className="button" >Html/Css/JS</button>
+        <button className="button" >Python</button>
+      </div>
+
+      <div className="projects">
+        {projects.length > 0 ? projects[0].map((project, i) => {
+          return <Card
+            key={i}
+            title={project.title}
+            url={project.url}
+            description={project.description}
+            lifeWeb={project.lifeWeb}
+            img={project.img}
+            category={project.category}
+          />
+        }) :
+          null
+        }
+        {/* <Particles
+        className="particles"
+        params={particulesOptions}
+      /> */}
+      </div>
     </div>
   );
 }
